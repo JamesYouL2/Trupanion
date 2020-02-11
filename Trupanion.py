936d9d9c5fa30e239c9280e3e6ee58828df071aa
James <james.you@l2political.com>
2020-02-11 13:30:11 -0800

    Update scripts

--- Trupanion.py
+++ Trupanion.py
@@ -1,36 +1,35 @@
+#Import libraries
 import pandas as pd
 from datetime import datetime
 from lightgbm import LGBMClassifier, LGBMRegressor
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error, roc_curve, auc
 import shap
-from sklearn.metrics import mean_squared_error
 from math import sqrt
 
-#Import Data
+#Import Data after preprocessed in SQL
 df=pd.read_csv('C:/Trupanion/MonthsPetData.txt',sep='\t',parse_dates=['MinDate'])
 
+#Fill dependent variable with 0 where it is null
 df['ClaimAmount']=df['ClaimAmount'].fillna(0)
-df['ClaimAmountPast']=df['ClaimAmountPast'].fillna(0)
-df['ClaimAmountPastMonth']=df['ClaimAmountPastMonth'].fillna(0)
-df['ClaimAmountPastYear']=df['ClaimAmountPastYear'].fillna(0)
-df['ClaimAmountAverage']=df['ClaimAmountAverage'].fillna(0)
-df['DaysInPolicy']=df['DaysInPolicy'].fillna(0)
 
 #Split Data into X and Y
-filter_col = [col for col in df if col.startswith('ClaimAmount')]
+#Get Valid Columns
+filter_col = [col for col in df if col.startswith('Claim') or col.startswith('Past')]
 filter_col.remove('ClaimAmount')
-X1=df[['MonthNumber','MonthYear','MinDate','TotalDays','DaysInPolicy','Species','Breed','AgeAtEnroll','MinAgeInDays','MaxAgeInDays','TotalDaysInPolicy']]
+X1=df[['MonthNumber','MonthYear','MinDate','TotalDays','DaysInPolicy','Species','Breed','AgeAtEnroll','MinAgeInDays','MaxAgeInDays','TotalDaysInPolicy','TotalMonthsInPolicy']]
 X2=df[filter_col]
 XCombined = X1.join(X2)
 
+#One Hot Encode categorical variables
 X = pd.get_dummies(XCombined, prefix_sep="_", columns=['Breed','Species','AgeAtEnroll'])
 
-y=df[['MinDate','ClaimAmount']]
+y=df[['MinDate','ClaimAmount','PetId']]
 
-#Note, test on previous year for accuracy
+#Create Train/Test Splits
+#Test on previous year for accuracy
 date=pd.Timestamp(2018,7,1)
 X_train=X.loc[X['MinDate']<date]
 y_train=y.loc[y['MinDate']<date]
@@ -39,14 +38,12 @@ X_test=X.loc[X['MinDate']==date]
 y_test=y.loc[y['MinDate']==date]
 
 X_train.drop('MinDate',axis='columns',inplace=True)
-y_train.drop('MinDate',axis='columns',inplace=True)
 X_test.drop('MinDate',axis='columns',inplace=True)
-y_test.drop('MinDate',axis='columns',inplace=True)
 
-#Uses Randomized Search CV to tune parameters
+#Use Randomized Search CV to tune parameters
 gridParams = {
     'max_depth': [4,8,16],
-    'min_child_samples': [10,20],
+    'min_child_samples': [20,60,100],
     'learning_rate': [0.005,0.01],
     'n_estimators': [40],
     'num_leaves': [16,32],
@@ -57,7 +54,6 @@ gridParams = {
     'reg_lambda' : [1,1.2,1.4]
     }
 
-#Run RandomizedSearch CV
 regressor = LGBMRegressor()
 
 grid = RandomizedSearchCV(regressor, gridParams,
@@ -66,30 +62,106 @@ grid = RandomizedSearchCV(regressor, gridParams,
                     n_jobs=8,
                     n_iter=50)
 
-grid.fit(X_train, y_train)
+grid.fit(X_train, y_train['ClaimAmount'])
 
 # Print the best parameters found
 print(grid.best_params_)
 print(grid.best_score_)
 
-#Use best params
+#Run Regression using best params
 regressor = LGBMRegressor(**grid.best_params_)
 
-regressor.fit(X=X_train,y=y_train) #training the algorithm
+regressor.fit(X=X_train,y=y_train['ClaimAmount']) #training the algorithm
 
 y_pred = regressor.predict(X_test)
 
-print(r2_score(y_test,y_pred))#0.01
-print(sqrt(mean_squared_error(y_test, y_pred)))
+#Use Normalized Gini as a parameter (https://www.kaggle.com/oxofff/gini-scorer-cv-gridsearch)
+def gini(solution, submission):
+    df = zip(solution, submission, range(len(solution)))
+    df = sorted(df, key=lambda x: (x[1],-x[2]), reverse=True)
+    rand = [float(i+1)/float(len(df)) for i in range(len(df))]
+    totalPos = float(sum([x[0] for x in df]))
+    cumPosFound = [df[0][0]]
+    for i in range(1,len(df)):
+        cumPosFound.append(cumPosFound[len(cumPosFound)-1] + df[i][0])
+    Lorentz = [float(x)/totalPos for x in cumPosFound]
+    Gini = [Lorentz[i]-rand[i] for i in range(len(df))]
+    return sum(Gini)
 
+def normalized_gini(solution, submission):
+    normalized_gini = gini(solution, submission)/gini(solution, solution)
+    return normalized_gini
+
+#Compare normalized gini and Mean Squared Error
+#Use mean as a benchmark
 y_test['ClaimAmountAvg']=y_test.mean().values[0]
 
-print(sqrt(mean_squared_error(y_test['ClaimAmount'], y_test_mean['ClaimAmountAvg'])))
+print(normalized_gini(y_test['ClaimAmount'], y_pred))#0.679
+print(sqrt(mean_squared_error(y_test['ClaimAmount'], y_pred)))#706
 
-print(y_test.sum())
-print(y_pred.sum())
+print(normalized_gini(y_test['ClaimAmount'], y_test['ClaimAmountAvg']))#0.075
+print(sqrt(mean_squared_error(y_test['ClaimAmount'], y_test['ClaimAmountAvg'])))#711
+#Mean Squared Error not much improved, but normalized gini much improved
 
 #Use shap to plot LGBM Model
 explainer = shap.TreeExplainer(regressor)
 shap_values = explainer.shap_values(X_test)
 shap.summary_plot(shap_values, X_test, plot_type="bar")
+#Past values most important for prediction
+
+############
+##Real Run
+############
+#Create Train/Test Splits
+#Test on current year
+date=pd.Timestamp(2019,7,1)
+X_train=X.loc[X['MinDate']<date]
+y_train=y.loc[y['MinDate']<date]
+
+X_test=X.loc[X['MinDate']==date]
+y_test=y.loc[y['MinDate']==date]
+
+X_train.drop('MinDate',axis='columns',inplace=True)
+X_test.drop('MinDate',axis='columns',inplace=True)
+
+#Use Randomized Search CV to tune parameters
+gridParams = {
+    'max_depth': [4,8,16],
+    'min_child_samples': [20,60,100],
+    'learning_rate': [0.005,0.01],
+    'n_estimators': [40],
+    'num_leaves': [16,32],
+    'boosting_type' : ['gbdt'],
+    'colsample_bytree' : [0.6, 0.65, 0.7],
+    'subsample' : [0.7,0.75],
+    'reg_alpha' : [1,1.2],
+    'reg_lambda' : [1,1.2,1.4]
+    }
+
+regressor = LGBMRegressor()
+
+grid = RandomizedSearchCV(regressor, gridParams,
+                    verbose=0,
+                    cv=5,
+                    n_jobs=8,
+                    n_iter=50)
+
+grid.fit(X_train, y_train['ClaimAmount'])
+
+#Run Regression using best params
+regressor = LGBMRegressor(**grid.best_params_)
+
+regressor.fit(X=X_train,y=y_train['ClaimAmount']) #training the algorithm
+
+y_pred = regressor.predict(X_test)
+
+#Use shap to plot LGBM Model
+explainer = shap.TreeExplainer(regressor)
+shap_values = explainer.shap_values(X_test)
+shap.summary_plot(shap_values, X_test, plot_type="bar")
+#Past values most important for prediction
+
+#Output Final Predictions
+y_test['pred'] = y_pred
+final=pd.merge(df[['PetId']].drop_duplicates('PetId'),y_test,on='PetId',how='left').fillna(0)
+final[['PetId','pred']].to_csv('predictions.csv',header=True,index=False)
--- Trupanion.sql
+++ Trupanion.sql
@@ -24,10 +24,16 @@ ClaimAmount float)
 truncate table petdata
 truncate table claimdata
 
+--Import Data
 --Note, eliminated filepath here because of possible confidentiality
 BULK INSERT PetData from 'petdata.csv' with (FIELDTERMINATOR = ',',FIRSTROW=2)
 BULK INSERT ClaimData from 'claimdata.csv' with (FIELDTERMINATOR = ',',FIRSTROW=2)
 
+------------------
+--Feature Engineering
+------------------
+
+--Check Dates for PetData/ClaimData
 select min(enrolldate) from petdata--2009-11-20
 select min(CancelDate) from petdata--2010-01-13
 select max(enrolldate) from petdata--2019-11-27
@@ -36,6 +42,28 @@ select max(CancelDate) from petdata--2019-11-27
 select min(ClaimDate) from claimdata--2010-01-06
 select max(ClaimDate) from claimdata--2019-06-30
 
+--Get Pet Birthdates
+--Since we have ranges, get mininum possible birthdate and maximum possible birthdate
+alter table petdata add MinBirthDate date
+alter table petdata add MaxBirthDate date
+alter table petdata add YearsAtEnroll int
+GO
+SELECT COUNT(*), AgeAtEnroll from petdata group by AgeAtEnroll
+
+update petdata set YearsAtEnroll = LEFT(AgeAtEnroll, charindex(' ', AgeAtEnroll) - 1) where AgeAtEnroll like '%year%'
+
+update petdata set maxBirthDate = DATEADD(year,YearsAtEnroll*-1,EnrollDate)
+update petdata set minBirthDate = DATEADD(year,(1+YearsAtEnroll)*-1,EnrollDate)
+
+update petdata set minBirthDate = DATEADD(month,-12,EnrollDate) where AgeAtEnroll = '8 weeks to 12 months old'
+update petdata set maxBirthDate = DATEADD(week,-8,EnrollDate) where AgeAtEnroll = '8 weeks to 12 months old'
+
+update petdata set minBirthDate = DATEADD(week,-7,EnrollDate) where AgeAtEnroll = '0-7 weeks old'
+update petdata set maxBirthDate = EnrollDate where AgeAtEnroll = '0-7 weeks old'
+
+------------------
+--Get PetData split up by month
+------------------
 --Get all months and year between 2010-01-01 and 2019-06-30
 drop table if exists months
 
@@ -69,44 +97,24 @@ OPTION(MAXRECURSION 0)
 delete from Months where MonthYear > 2019
 delete from Months where MonthYear = 2019 and MonthNumber > 7
 
---Get Pet Birthdates
---Since we have ranges, get mininum possible birthdate and maximum possible birthdate
-alter table petdata add MinBirthDate date
-alter table petdata add MaxBirthDate date
-alter table petdata add YearsAtEnroll int
-GO
-SELECT COUNT(*), AgeAtEnroll from petdata group by AgeAtEnroll
-
-update petdata set YearsAtEnroll = LEFT(AgeAtEnroll, charindex(' ', AgeAtEnroll) - 1) where AgeAtEnroll like '%year%'
-
-update petdata set maxBirthDate = DATEADD(year,YearsAtEnroll*-1,EnrollDate)
-update petdata set minbirthdate = DATEADD(year,(1+YearsAtEnroll)*-1,EnrollDate)
-
-update petdata set minbirthdate = DATEADD(month,-12,EnrollDate) where AgeAtEnroll = '8 weeks to 12 months old'
-update petdata set maxBirthDate = DATEADD(week,-8,EnrollDate) where AgeAtEnroll = '8 weeks to 12 months old'
-
-update petdata set minbirthdate = DATEADD(week,-7,EnrollDate) where AgeAtEnroll = '0-7 weeks old'
-update petdata set maxBirthDate = EnrollDate where AgeAtEnroll = '0-7 weeks old'
-
 --Combine Pet Data with Months
 drop table if exists MonthsPetData
 
 select * into MonthsPetData from months m full outer join petdata p on MaxDate >= EnrollDate and (CancelDate >= MinDate or CancelDate is null)
 
---check to make sure you're getting days in Policy each month right
-select *, DATEDIFF(DAY,EnrollDate,MinDate) from MonthsPetData where EnrollDate > MinDate
-select *, DATEDIFF(DAY,MaxDate,CancelDate) from MonthsPetData where MaxDate > CancelDate
-
 alter table MonthsPetData add DaysInPolicy int
 GO
 
---Make sure to get Days in Policy (most of the time it's the same as the number of days in month, but have to adjust for enroll/cancel date
+-----------------
+--Feature Engineering for PetData by Month
+-----------------
+
+--Get Days in Policy for each pet-month combination (most of the time it's the same as the number of days in month, but have to adjust for enroll/cancel date
 update MonthsPetData set DaysInPolicy = DaysInMonth
 update MonthsPetData set DaysInPolicy = DaysInPolicy + DATEDIFF(DAY,EnrollDate,MinDate) where EnrollDate > MinDate
 update MonthsPetData set DaysInPolicy = DaysInPolicy + DATEDIFF(DAY,MaxDate,CancelDate) where MaxDate > CancelDate
 
-
---Get possible age ranges for each month
+--Get possible age ranges for each pet-month combination
 alter table MonthsPetData add MinAgeInDays int
 alter table MonthsPetData add MaxAgeInDays int
 GO
@@ -119,69 +127,139 @@ from MonthsPetData where DATEDIFF(DAY,MaxBirthDate,MinDate) < 0
 update MonthsPetData set MaxAgeInDays = DATEDIFF(DAY,MinBirthDate,MaxDate)
 update MonthsPetData set MinAgeInDays = DATEDIFF(DAY,MaxBirthDate,MinDate)
 
---Features to see if days from beginning or end of policy matter
+--Get number of months and days from start of policy
 alter table MonthsPetData add TotalDaysInPolicy int
-alter table MonthsPetData add DaysToEndOfPolicy int
 alter table MonthsPetData add TotalMonthsInPolicy int
 GO
 update MonthsPetData set TotalMonthsInPolicy = DATEDIFF(MONTH,EnrollDate,MaxDate)
 update MonthsPetData set TotalDaysInPolicy = DATEDIFF(DAY,EnrollDate,MaxDate)
-update MonthsPetData set DaysToEndOfPolicy = DATEDIFF(DAY,MinDate,CancelDate)
 
-drop table if exists #claimDataGroup, #PastClaims
+drop table if exists #ClaimDataGroup, #PastClaims
 
---Group Policies by Month
+--Get Number of Claims and Claim Amounts for month-pet combiatino
 select PetID, count(*) as NumberOfClaims, Month(ClaimDate) as ClaimMonth, year(ClaimDate) as ClaimYear, sum(ClaimAmount) as ClaimAmount, cast(cast(Month(claimDate) as varchar)+'/1/'+cast(Year(ClaimDate) as varchar) as date) as ClaimDate
 into #ClaimDataGroup
 from claimdata
 GROUP BY PetID, Month(ClaimDate), year(ClaimDate)
 
-select * from #ClaimDataGroup
-
 --select * from #ClaimDataGroup c join MonthsPetData m on MonthNumber = ClaimMonth and MonthYear=ClaimYear and c.PetID = m.PetID
-
---Get Claim Amount
+----------------------------------
+--Get Claim Amount (dependent variable)
+----------------------------------
 alter table MonthsPetData add ClaimAmount float
 GO
 update m set ClaimAmount = c.claimAmount from
 #ClaimDataGroup c join MonthsPetData m on MonthNumber = ClaimMonth and MonthYear=ClaimYear and c.PetID = m.PetID
 
+------------------------------
+--Get Previous Months Claim Amount for each pet
+------------------------------
 select c.PetID, c.ClaimAmount, c.ClaimDate, m.minDate, DateDIFF(Month,c.ClaimDate,m.MinDate) as MonthDiff, NumberOfClaims
 into #PastClaims
 from #ClaimDataGroup c join MonthsPetData m on c.PetID = m.PetId and m.mindate > c.ClaimDate
 
-select * from #PastClaims order by PetID, monthdiff
+drop table if exists ClaimRanking
+
+------------------------------
+--Get Size and how long ago each claim for
+------------------------------
+select c.PetID, c.ClaimDate, c.ClaimAmount, m.MinDate, ROW_NUMBER() over (PARTITION BY c.PetID, m.Mindate ORDER by ClaimDate desc, ClaimID) as ClaimRank, 
+DATEDIFF(Day,ClaimDate, MinDate) as DateDif
+into ClaimRanking
+from claimdata c join MonthsPetData m on c.PetID = m.PetID and m.mindate > c.ClaimDate
+
+--select * from #PastClaims order by PetID, monthdiff
 
 --Get Claim Amounts from the past
 alter table MonthsPetData add ClaimAmountPast float
 alter table MonthsPetData add ClaimAmountAverage float
+alter table MonthsPetData add ClaimNumber float
+alter table MonthsPetData add ClaimAverageSize float
 GO
+--Get total of all claims in the past
 update MonthsPetData set ClaimAmountPast = t.ClaimAmountPast from (
 select sum(claimamount) as ClaimAmountPast, minDate, PetID from #PastClaims p group by Mindate, PetID) t 
 join MonthsPetData m on t.PetID = m.PetID and t.MinDate = m.Mindate
 
+--Get Average claim amount per month of claims in the past
 update MonthsPetData set ClaimAmountAverage = ClaimAmountPast / cast(TotalMonthsInPolicy as float)
+
+--Get Number of Claims in the past
+update MonthsPetData set ClaimNumber = t.ClaimNumber from (
+select sum(NumberOfClaims) as ClaimNumber, minDate, PetID from #PastClaims p group by Mindate, PetID) t 
+join MonthsPetData m on t.PetID = m.PetID and t.MinDate = m.Mindate
+
+--Get Average Size for claims in the past
+update MonthsPetData set ClaimAverageSize = ClaimAmountPast / ClaimNumber
+
 /*
 update MonthsPetData set ClaimAmountPastMonth = t.ClaimAmountPast from (
 select sum(claimamount) as ClaimAmountPast, minDate, PetID from #PastClaims p where MonthDiff = 1 group by Mindate, PetID) t 
 join MonthsPetData m on t.PetID = m.PetID and t.MinDate = m.Mindate
 */
 
+--Get total of all claims in the past X months (between 1 and 12)
+--Get number of all claims in the past X months (between 1 and 12)
+--Get average size of all claims in the past X months (between 1 and 12)
+--Get % change between Average Claim Amount overall and Average Claim Amount in the past 
 DECLARE @SQL VARCHAR(MAX)
 DECLARE @loopcounter int = 1
-WHILE @loopcounter <= 24
+WHILE @loopcounter <= 12
 BEGIN
-SET @SQL = 'Alter table MonthsPetData add ClaimAmountPast'+cast(@loopcounter as varchar)+'MonthOrMore float'
+SET @SQL = 'Alter table MonthsPetData add ClaimAmountPast'+cast(@loopcounter as varchar)+'MonthOrMore float
+Alter table MonthsPetData add ClaimNumberPast'+cast(@loopcounter as varchar)+'MonthOrMore int
+Alter table MonthsPetData add ClaimSizePast'+cast(@loopcounter as varchar)+'MonthOrMore float
+Alter table MonthsPetData add ClaimDeltaPast'+cast(@loopcounter as varchar)+'MonthOrMore float
+'
 EXEC(@SQL)
 SET @SQL ='update MonthsPetData set ClaimAmountPast'+cast(@loopcounter as varchar)+'MonthOrMore = t.ClaimAmountPast from (
 select sum(claimamount) as ClaimAmountPast, minDate, PetID from #PastClaims p where MonthDiff <= '+cast(@loopcounter as varchar)+' group by Mindate, PetID) t 
 join MonthsPetData m on t.PetID = m.PetID and t.MinDate = m.Mindate
+
+update MonthsPetData set ClaimNumberPast'+cast(@loopcounter as varchar)+'MonthOrMore = t.ClaimNumber from (
+select sum(NumberOfClaims) as ClaimNumber, minDate, PetID from #PastClaims p where MonthDiff <= '+cast(@loopcounter as varchar)+' group by Mindate, PetID) t 
+join MonthsPetData m on t.PetID = m.PetID and t.MinDate = m.Mindate
+
+update MonthsPetData set ClaimSizePast'+cast(@loopcounter as varchar)+'MonthOrMore 
+= ClaimAmountPast'+cast(@loopcounter as varchar)+'MonthOrMore / 
+cast(ClaimNumberPast'+cast(@loopcounter as varchar)+'MonthOrMore as float)
+from MonthsPetData
+where ClaimAmountPast'+cast(@loopcounter as varchar)+'MonthOrMore is not null
+
+update MonthsPetData set ClaimDeltaPast'+cast(@loopcounter as varchar)+'MonthOrMore 
+= ((ClaimAmountPast'+cast(@loopcounter as varchar)+'MonthOrMore /
+cast('+cast(@loopcounter as varchar)+' as float))-ClaimAmountAverage)
+/ cast(ClaimAmountAverage as float)
+from MonthsPetData
+where ClaimAmountPast'+cast(@loopcounter as varchar)+'MonthOrMore is not null
 '
+PRINT(@SQL)
 EXEC (@SQL)
 SET @loopcounter = @loopcounter + 1
+END--7m
+
+--Get Past 5 Claim Amounts and how long ago they were from each month-pet combination
+DECLARE @SQL2 VARCHAR(MAX)
+DECLARE @loopcounter2 int = 1
+WHILE @loopcounter2 <= 5
+BEGIN
+SET @SQL2 = 'Alter table MonthsPetData add PastAmount'+cast(@loopcounter2 as varchar)+'Claim float
+Alter table MonthsPetData add PastDay'+cast(@loopcounter2 as varchar)+'Claim int
+'
+EXEC(@SQL2)
+SET @SQL2 ='update MonthsPetData set PastAmount'+cast(@loopcounter2 as varchar)+'Claim = c.ClaimAmount from 
+ClaimRanking c join MonthsPetData m on c.PetID = m.PetID and c.MinDate = m.MinDate and ClaimRank = '+cast(@loopcounter2 as varchar)+'
+
+update MonthsPetData set PastDay'+cast(@loopcounter2 as varchar)+'Claim = c.DateDif from 
+ClaimRanking c join MonthsPetData m on c.PetID = m.PetID and c.MinDate = m.MinDate and ClaimRank = '+cast(@loopcounter2 as varchar)+'
+'
+PRINT(@SQL2)
+EXEC (@SQL2)
+SET @loopcounter2 = @loopcounter2 + 1
 END--7m
 
 --Export MonthsPetData
 select * from MonthsPetData order by claimamount desc
 
-exec SimpleBCP @SQLCommand = 'Select * from monthsPetData', @FileLocation = '\\ls02\telcom\James\Trupanion\MonthsPetData.txt'
+--Export MonthsPetData (Internal Export Proc)
+exec SimpleBCP @SQLCommand = 'Select * from MonthsPetData', @FileLocation = 'C:\Trupanion\MonthsPetData.txt'
